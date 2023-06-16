385d747caa71f2907a9469800d87da6478354a1e
seboo <25958061+seboo@users.noreply.github.com>
2023-06-16 14:44:55 +0200

    Update Http transport

--- src/java/fr/paris/lutece/plugins/identityquality/v3/web/rs/service/IdentityQualityTransportRest.java
+++ src/java/fr/paris/lutece/plugins/identityquality/v3/web/rs/service/IdentityQualityTransportRest.java
@@ -38,9 +38,11 @@ import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.crud.SuspiciousIdenti
 import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.duplicate.DuplicateRuleSummarySearchResponse;
 import fr.paris.lutece.plugins.identitystore.v3.web.rs.service.AbstractTransportRest;
 import fr.paris.lutece.plugins.identitystore.v3.web.rs.util.Constants;
-import fr.paris.lutece.plugins.identitystore.v3.web.service.HttpAccessTransport;
 import fr.paris.lutece.plugins.identitystore.v3.web.service.IHttpTransportProvider;
 import fr.paris.lutece.plugins.identitystore.web.exception.IdentityStoreException;
+import fr.paris.lutece.portal.service.util.AppException;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import java.util.HashMap;
@@ -61,14 +63,6 @@ public class IdentityQualityTransportRest extends AbstractTransportRest implemen
     private String _strIdentityStoreQualityEndPoint;
 
     /**
-     * Simple Constructor
-     */
-    public IdentityQualityTransportRest( )
-    {
-        super( new HttpAccessTransport( ) );
-    }
-
-    /**
      * Constructor with IHttpTransportProvider parameter
      *
      * @param httpTransport
@@ -77,17 +71,8 @@ public class IdentityQualityTransportRest extends AbstractTransportRest implemen
     public IdentityQualityTransportRest( final IHttpTransportProvider httpTransport )
     {
         super( httpTransport );
-    }
-
-    /**
-     * setter of identityStoreQualityEndPoint
-     *
-     * @param strIdentityStoreQualityEndPoint
-     *            value to use
-     */
-    public void setIdentityStoreQualityEndPoint( final String strIdentityStoreQualityEndPoint )
-    {
-        this._strIdentityStoreQualityEndPoint = strIdentityStoreQualityEndPoint;
+        
+        _strIdentityStoreQualityEndPoint = httpTransport.getApiEndPointUrl( );
     }
 
     /**
@@ -98,7 +83,7 @@ public class IdentityQualityTransportRest extends AbstractTransportRest implemen
     {
         _logger.debug( "Get duplicate rules of " + strApplicationCode );
 
-        this.checkClientApplication( strApplicationCode );
+        this.checkClientCode( strApplicationCode );
 
         final Map<String, String> mapHeadersRequest = new HashMap<>( );
         mapHeadersRequest.put( Constants.PARAM_CLIENT_CODE, strApplicationCode );
@@ -164,6 +149,22 @@ public class IdentityQualityTransportRest extends AbstractTransportRest implemen
                 mapParams, mapHeadersRequest, SuspiciousIdentitySearchResponse.class, _mapper );
 
         return response;
+    }
+    
+    /**
+     * check whether the parameters related to the identity are valid or not
+     *
+     * @param strClientCode
+     *            the strClientCode
+     * @throws AppException
+     *             if the parameters are not valid
+     */
+    public void checkClientCode( String strClientCode ) throws IdentityStoreException
+    {
+        if ( StringUtils.isBlank( strClientCode ) )
+        {
+            throw new IdentityStoreException( "Client code is mandatory." );
+        }
     }
 
 }
