fc3b15517cee7f6c71869079e93ec94081d326c5
Loris Boiteux <lboiteux@yupiik.com>
2023-10-12 12:54:16 +0200

    LUT-24948 - format

--- src/java/fr/paris/lutece/plugins/identityquality/v3/web/rs/service/HttpApiManagerAccessTransport.java
+++ src/java/fr/paris/lutece/plugins/identityquality/v3/web/rs/service/HttpApiManagerAccessTransport.java
@@ -98,7 +98,6 @@ public final class HttpApiManagerAccessTransport extends HttpAccessTransport
     {
         String strToken = StringUtils.EMPTY;
 
-
         Map<String, String> mapHeadersRequest = new HashMap<String, String>( );
         Map<String, String> mapParams = new HashMap<String, String>( );
 
